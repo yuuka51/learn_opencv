550822882bfc0311e3e235c5db3164b33d419fc8
yuuun <161l003485124kd@gmail.com>
2024-02-22 21:03:51 +0900

    main add cat_face_for_imagefile

---
+++ cat_face/cat_face_for_imagefile.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import cv2 as cv
+import os
+
+def detect(imagefilename, cascadefilename):
+    srcimg = cv.imread(imagefilename)
+    if srcimg is None:
+        print('cannot load image')
+        sys.exit(-1)
+    dstimg = srcimg.copy()
+    cascade = cv.CascadeClassifier(cascadefilename)
+    if cascade.empty():
+        print('cannnot load cascade file')
+        sys.exit(-1)
+    objects = cascade.detectMultiScale(srcimg, 1.1, 3)
+    for (x, y, w, h) in objects:
+        print(x, y, w, h)
+        cv.rectangle(dstimg, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    return dstimg
+
+if __name__ == '__main__':
+    # image_folder内の複数の画像を読み取る
+    for filename in os.listdir('猫画像が入っているディレクトリ'):
+        print(filename)
+        result = detect('猫画像が入っているディレクトリ' + filename, 'cascade.xmlのパス')
+        cv.imwrite(f"result_{filename}".format(filename), result)
